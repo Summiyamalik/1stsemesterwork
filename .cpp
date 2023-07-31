46c26f3a85cf18288e32cd08f6d4d964e72bb962
summayya <117230734+Summiyamalik@users.noreply.github.com>
2023-07-31 14:13:35 +0500

    Add files via upload

---
+++ .cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(void){
+	float c,f;
+	cout <<"enter the value of temperature in celsius: ";
+	cin >>c;
+	f=9/5*c+32;
+	cout <<"temperature in fahrenheit " <<f;
+	return 0;
+}
