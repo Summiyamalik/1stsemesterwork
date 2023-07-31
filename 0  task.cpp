6fa1a92b3af576817171f5e8c689a2fbe9907feb
summayya <117230734+Summiyamalik@users.noreply.github.com>
2023-07-31 14:14:06 +0500

    Add files via upload

---
+++ 0  task.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int i;
+    float mark, sum=0, per;
+    cout<<"Enter your five subjects Marks: ";
+    for(i=0; i<5; i++)
+    {
+        cin>>mark;
+        sum = sum+mark;
+    }
+    per = sum/5;
+    cout<<"\nGrade = ";
+    if(per>=75)
+        cout<<"A";
+    else if(per<75 && per>=60)
+        cout<<"B";
+    else if(per<60 && per>=45)
+        cout<<"C";
+    else if(per<30)
+        cout<<"D";
+    else
+        cout<<"Invalid!";
+    cout<<endl;
+    return 0;
+}
